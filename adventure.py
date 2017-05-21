7d883ae5af680c6397596879022e6bfacdf3b814
Chris Bradfield <chris@kidscancode.org>
2017-05-21 11:35:41 -0700

    edits

--- adventure.py
+++ adventure.py
@@ -1,13 +1,15 @@
 import pygame
 import random
 from os import path
+import xml.etree.ElementTree as ET
 game_folder = path.dirname(__file__)
 map_folder = path.join(game_folder, 'maps')
+art_folder = path.join(game_folder, 'art')
 
 WIDTH = 800
 HEIGHT = 640
 FPS = 60
-TILESIZE = 32
+TILESIZE = 64
 PLAYER_ACCEL = 5000  # pixels/sec
 FRICTION = -15
 
@@ -23,6 +25,33 @@ pygame.display.set_caption("Adventure!")
 clock = pygame.time.Clock()
 
 vec2 = pygame.math.Vector2
+
+class Spritesheet:
+    def __init__(self, filename):
+        # filename - without extension, find matching xml
+        self.spritesheet = pygame.image.load(filename + '.png').convert_alpha()
+        if path.isfile(filename + '.xml'):
+            tree = ET.parse(filename + '.xml')
+            self.map = {}
+            # read xml and pull out desired values (x, y, w, h)
+            for node in tree.iter():
+                if node.attrib.get('name'):
+                    name = node.attrib.get('name')
+                    self.map[name] = {}
+                    self.map[name]['x'] = int(node.attrib.get('x'))
+                    self.map[name]['y'] = int(node.attrib.get('y'))
+                    self.map[name]['w'] = int(node.attrib.get('width'))
+                    self.map[name]['h'] = int(node.attrib.get('height'))
+
+    def get_image_by_name(self, name):
+        rect = pygame.Rect(self.map[name]['x'], self.map[name]['y'],
+                           self.map[name]['w'], self.map[name]['h'])
+        return self.spritesheet.subsurface(rect)
+
+    def get_image_by_rect(self, x, y, w, h):
+        rect = pygame.Rect(x, y, w, h)
+        return self.spritesheet.subsurface(rect)
+
 class Camera:
     def __init__(self, width, height):
         self.camera = pygame.Rect(0, 0, width, height)
@@ -52,21 +81,33 @@ class Wall(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((TILESIZE//1.2, TILESIZE//1.2 ))
-        self.image.fill(YELLOW)
+        #self.image = pygame.Surface((TILESIZE//1.2, TILESIZE//1.2 ))
+        self.image = character_sheet.get_image_by_name('manBlue_gun.png')
+        self.image_clean = self.image.copy()
         self.rect = self.image.get_rect()
         self.pos = vec2(x, y)
         self.vel = vec2(0, 0)
         self.acc = vec2(0, 0)
+        self.rot = 0
         self.rect.center = self.pos
 
     def update(self, dt):
         self.acc = vec2(0, 0)
+        self.rot_speed = 0
         keys = pygame.key.get_pressed()
-        self.acc.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
-        self.acc.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
-        if self.acc.length() > 0:
-            self.acc = self.acc.normalize() * PLAYER_ACCEL
+        if keys[pygame.K_LEFT]:
+            self.rot_speed = 200
+        if keys[pygame.K_RIGHT]:
+            self.rot_speed = -200
+        if keys[pygame.K_UP]:
+            self.acc = vec2(PLAYER_ACCEL, 0).rotate(-self.rot)
+        # self.acc.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        # self.acc.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+        # if self.acc.length() > 0:
+        #     self.acc = self.acc.normalize() * PLAYER_ACCEL
+        self.rot = (self.rot + self.rot_speed * dt) % 360
+        self.image = pygame.transform.rotate(self.image_clean, self.rot)
+        self.rect = self.image.get_rect(center=self.rect.center)
         self.acc += self.vel * FRICTION
         self.vel += self.acc * dt
         self.pos += self.vel * dt
@@ -109,6 +150,7 @@ walls = pygame.sprite.Group()
 # wall1 = Wall(32 * 10, 32 * 10)
 # all_sprites.add(wall1)
 # walls.add(wall1)
+character_sheet = Spritesheet(path.join(art_folder, 'spritesheet_characters'))
 map_data = []
 with open(path.join(map_folder, 'map4.txt'), 'rt') as datafile:
     for line in datafile:
@@ -138,7 +180,7 @@ while running:
     screen.fill(BLACK)
     #all_sprites.draw(screen)
     for sprite in all_sprites:
-       screen.blit(sprite.image, camera.apply(sprite))
+        screen.blit(sprite.image, camera.apply(sprite))
     pygame.display.flip()  # last
 
 pygame.quit()
